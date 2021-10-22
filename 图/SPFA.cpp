6a74f83e8254b6ac0f7175cb25869c80d782190e
13toast <lighthouse117@163.com>
2021-10-22 20:45:39 +0800

    change spfa

--- 图/SPFA.cpp
+++ 图/SPFA.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 typedef long long ll;
 typedef pair<int, int> pii;
-const int MAXN = 100010;
+const int maxn = 100010;
 const ll INF = 0x7fffffff;
 const double eps = 1e-8;
 const int mod = 1e7+7;
@@ -11,36 +11,39 @@ const int mod = 1e7+7;
 vector<pii> vec[MAXN];
 queue<pii> Q;
 
-int dis[MAXN], cnt[MAXN]; //cnt记录每个点入队次数,==n说明有负环
+int dis[MAXN], cnt[MAXN]; //cnt记录每个点入队次数,>=n说明有负环
 bool vis[MAXN];			  //是否进队
 int n, m, S;
 
+struct edge {
+  int v, w;
+};
 bool SPFA() {
-	memset(dis,0x3f,sizeof(dis));
-	memset(vis,0,sizeof(vis));
-	memset(cnt,0,sizeof(cnt));
-	Q.push(make_pair(0, S));
-	dis[S] = 0;
-	cnt[S]++;
-	while (!Q.empty()) {
-		int u = Q.front().second;
-		Q.pop();
-		vis[u] = 0;
-		for (int i=0;i<(int)vec[u].size();++i) {
-			int v = vec[u][i].second, w = vec[u][i].first;
-			if (dis[u] + w < dis[v]) {
-				dis[v] = dis[u] + w;
-				cnt[v]++;
-				if (cnt[v] >= n) return true;
-				if(!vis[v]) {    //如果不在队列内
-					Q.push(make_pair(dis[v], v));
- 					vis[v] = true;
- 				}
- 			}
 
-		}
-	}
-	return false;
+vector<edge> e[maxn];
+int dis[maxn], cnt[maxn], vis[maxn];
+queue<int> q;
+bool spfa(int n, int s) {
+  memset(dis, 63, sizeof(dis));
+  dis[s] = 0, vis[s] = 1;
+  q.push(s);
+  while (!q.empty()) {
+    int u = q.front();
+    q.pop(), vis[u] = 0;
+    for (auto ed : e[u]) {
+      int v = ed.v, w = ed.w;
+      if (dis[v] > dis[u] + w) {
+        dis[v] = dis[u] + w;
+        cnt[v] = cnt[u] + 1;  // 记录最短路经过的边数
+        if (cnt[v] >= n) return false;
+        // 在不经过负环的情况下，最短路至多经过 n - 1 条边
+        // 因此如果经过了多于 n 条边，一定说明经过了负环
+        if (!vis[v]) q.push(v), vis[v] = 1;
+      }
+    }
+  }
+  return true;
+}
 }
 int main() {
 	scanf("%d%d%d", &n, &m,&S);
