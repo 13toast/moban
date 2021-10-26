e838239e4123c7c11751627fb8acb8f2606671d5
13toast <lighthouse117@163.com>
2021-10-26 20:52:25 +0800

    更新线性基

--- 小技巧/线性基.cpp
+++ 小技巧/线性基.cpp
@@ -1,21 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-ll a[105];
-
-ll lb[62];
+ll a[105],lb[62];
 inline void insert(ll x) {
 	for (int i=60;i+1;--i) {
 		if (!(x>>i)) continue;
 		if (!lb[i]) { lb[i] = x; break; }
 		x = x^lb[i];
 	}
+	//if (x) return false; 不能插入,表示已经能被表示
 }
 ll get_max() {
 	ll ret = 0;
 	for (int i=60;i+1;--i) 
 		ret = ((ret^lb[i])>ret)?ret^lb[i]:ret;
 	return ret;
+}
+void get_kth() { //第k小
+	ll LB[62] = {};
+	int cnt = 0;
+	rep(i,0,60) if (lb[i]) LB[++cnt] = lb[i];
+	int q; scanf("%d",&q);
+	rep(i,1,q) {
+		ll k,t = 0; scanf("%lld",&k);
+		if (k > (1<<cnt)-1) { printf("-1\n"); continue;}
+		ll ans = 0;
+		while (k) {
+			ans ^= (x&1)*LB[t++];
+			k >>= 1;
+		}
+		printf("%lld\n",ans);
+	} 
 }
 int main() {
 	int n;
@@ -25,5 +40,7 @@ int main() {
 		insert(a[i]);
 	}
 	cout << get_max() << endl;
+	get_kth();
+	count_k();
 	return 0;
 }
